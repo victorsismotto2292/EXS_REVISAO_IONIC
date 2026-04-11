123d6635282eaec794a47aa531d9ca129888a105
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-04-11 16:24:32 -0300

    Reinstalando pasta exs_main
    Por favor desbugar

---
+++ exs_main/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'exs_main',
+  webDir: 'www'
+};
+
+export default config;
